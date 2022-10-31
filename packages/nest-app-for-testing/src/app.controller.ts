a4de79047ddf7d6aa550c72a2331773c412f49d9
maple5233 <326153996@qq.com>
2022-10-31 12:48:48 +0800

    feat(nest-app): add dto for testing

--- packages/nest-app-for-testing/src/app.controller.ts
+++ packages/nest-app-for-testing/src/app.controller.ts
@@ -1,15 +1,24 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
-import { Response } from '@/app.dto'
+import { GetNormalResponse, HelloMessageResponse } from '@/app.dto'
+import { ApiResponse } from '@nestjs/swagger'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): Response {
+  getHello(): HelloMessageResponse {
     return {
       message: this.appService.getHello(),
     }
+  }
+
+  @Get('normal')
+  @ApiResponse({
+    type: GetNormalResponse,
+  })
+  getNormal() {
+    throw new Error('Not implemented')
   }
 }
--- packages/nest-app-for-testing/src/app.dto.ts
+++ packages/nest-app-for-testing/src/app.dto.ts
@@ -1,3 +1,48 @@
-export class Response {
+import { ApiProperty } from '@nestjs/swagger'
+
+export class HelloMessageResponse {
   message: string
+}
+
+export class GetNormalResponse {
+  @ApiProperty({
+    type: 'string',
+  })
+  message: string
+
+  @ApiProperty({
+    type: 'number',
+  })
+  code: number
+
+  @ApiProperty({
+    type: 'boolean',
+  })
+  success: boolean
+
+  @ApiProperty({
+    type: 'enum',
+    enum: ['A', 'B'],
+  })
+  enum: 'A' | 'B'
+
+  @ApiProperty({
+    type: 'array',
+    items: {
+      type: 'string',
+    },
+  })
+  stringArray: string[]
+
+  @ApiProperty({
+    type: 'object',
+    properties: {
+      name: {
+        type: 'string',
+      },
+    },
+  })
+  object: {
+    name: string
+  }
 }
