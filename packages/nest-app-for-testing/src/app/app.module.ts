7dc198337d303103443adeb3ea347a1e7d07a573
maple5233 <326153996@qq.com>
2022-11-13 02:36:38 +0800

    chore(testing): remove unused testing code

--- packages/nest-app-for-testing/src/app/app.module.ts
+++ packages/nest-app-for-testing/src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { ArrayController } from '@/array/array.controller'
+import { ObjectController } from '@/object/object.controller'
+import { NumberController } from '@/number/number.controller'
+import { StringController } from '@/string/string.controller'
+import { BoolController } from '@/boolean/bool.controller'
+import { OtherController } from '@/other/other.controller'
+import { EnumerableController } from '@/enumerable/enumerable.controller'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    NumberController,
+    BoolController,
+    StringController,
+    EnumerableController,
+    ArrayController,
+    ObjectController,
+    OtherController,
+  ],
 })
 export class AppModule {}
